74df4189f5dbd99529debb0394dc22d6792b75e1
LeuisKen <leuisken@gmail.com>
2015-11-21 09:51:02 +0800

    project init

---
+++ example/js/controller.js
@@ -0,0 +1,11 @@
+var homeController = function (model) {
+    model.name = 'home'
+}
+
+var postController = function (model) {
+    model.name = 'post'
+}
+
+var articleController = function (model) {
+    model.name = 'article'
+}
---
+++ example/js/route.js
@@ -0,0 +1,3 @@
+star.addRoute('home', 'view/templete.html', homeController)
+star.addRoute('post', 'view/templete.html', postController)
+star.addRoute('article', 'view/templete.html', articleController)
